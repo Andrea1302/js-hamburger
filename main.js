fbea3f44d92e8c3fb4537b109bfdad0daf5240cd
Andrea Scarpa <scarpandrea.1996@hotmail.com>
2021-10-22 16:16:13 +0200

    Aggiunto js al click hamburger menu e x ( close )

---
+++ main.js
@@ -0,0 +1,30 @@
+// Hamburger menu: mostrare / nascondere il menu principale
+
+
+// Richiamo la classe fas fa-bars per poter accedere al click 
+const MsHamburgerMenu = document.querySelector(".fa-bars");
+
+// Richiamo la classe hamburger-menu e creo la sua variabile 
+const hamburgerMenuClicked = document.querySelector(".hamburger-menu");
+
+// richiamo la classe close per creare la sua variabile 
+const chiudi = document.querySelector(".close");
+
+// Aggiungo comando click sull hamburger menu 
+    // fare in modo che al click dall hamburger menu si apra il suo sotto menu apposito
+
+MsHamburgerMenu.addEventListener('click',
+
+    function ( ) {
+        hamburgerMenuClicked.style.display="block";
+    }
+
+);
+chiudi.addEventListener('click',
+
+    function( ) {
+        hamburgerMenuClicked.style.display="none";
+    }
+
+)
+
